6199ccfe4834003dd910f5a4095667e13bceb454
18214027 - Ahmad Idham T Lubis <ofan.lubis@gmail.com>
2017-03-14 19:13:10 +0700

    ver 1.2

--- routes/home.js
+++ routes/home.js
@@ -7,15 +7,25 @@ router.use(bodyParser.urlencoded({ extended: true }));
 
 router.use('/', express.static('public'));
 
+var commonwords = ["aku", "asd"];
+
 router.post('/', function(req, res, next) {
 	
-	var arr = req.body.text.split(" ");
+	var arr = split2word(req.body.text);
 
 	arr.forEach(function(element, index, array){
 		array[index] = {"word": element, "tag": "O"}
 		
 		if (array[index].word.match(/^[A-Z]([a-z]|[.]$)/)){
-			array[index].tag = "NE";
+			if(array[index-1] != undefined){
+				if(array[(index-1)].word.match(/[.]$/)){
+					array[index].tag = "START";
+				} else {
+					array[index].tag = "NE";
+				}
+			} else {
+				array[index].tag = "START";
+			}
 		}
 	});
 	
@@ -23,9 +33,43 @@ router.post('/', function(req, res, next) {
 		arr.filter(function(value){
 			return value.tag != "O";
 		}).map(function(value){
-			return value.word;
+			return value;
 		})
 	);
 });
 
 module.exports = router;
+
+function split2char(str){
+	
+	var arr = str.split("");
+	return arr;
+}
+
+function split2word(str){
+	
+	var arrTemp = split2char(str);
+	var arr;
+	var word;
+	var i = 0;
+	
+	arrTemp.forEach(function(element){
+		if (element.match(" ")){
+			arr[i] = word;
+			i++;
+			word = "";
+		}
+		else{
+			if (element.match(/^([A-Z]|[a-z])/)){
+				word += element;
+			}
+			else{
+				arr[i] = word;
+				i++;
+				word = element;
+			}
+		}
+	});
+	
+	return arr;
+}
