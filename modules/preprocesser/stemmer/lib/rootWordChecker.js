b5a6c5e889f1146a0148720f2de69b8078e7227f
DXGeneral <ofan.lubis@gmail.com>
2017-06-17 19:35:47 +0700

    Idham - Suffix Stemmer - Masih skeleton dasar

--- modules/preprocesser/stemmer/lib/rootWordChecker.js
+++ modules/preprocesser/stemmer/lib/rootWordChecker.js
@@ -1,7 +1,18 @@
 var katadasar = require("../../../../models/kata-dasar.js").get;
 
 var rootWordChecker = function(word){
+	
+	var isFound = false;
+	var i = 0;
+	
+	while(i < katadasar.length && !isFound){
+		if(katadasar[i] === word.word){
+			isFound = true;
+		}
+	}
 
+	word.found = isFound;
+	
 	return word;
 }
 module.exports = rootWordChecker;
--- modules/preprocesser/stemmer/lib/suffixStemmer.js
+++ modules/preprocesser/stemmer/lib/suffixStemmer.js
@@ -1,6 +1,37 @@
 var katadasar = require("../../../../models/kata-dasar.js").get;
 
 var suffixStemmer = function(word){
+	/* 
+	Akan ada dua fase
+	Fase pertama adalah pengecekan dan penghapusan Inflection Suffix -lah dan -kah
+	Fase kedua adalah pengecekan dan penghapusan Inflectional Possessive Pronoun  (-ku, -mu, -nya)
+	Fase kedua selalu dijalankan setelah fase pertama
+	Fase kedua dapat dijalankan tanpa perduli dijalankan atau tidaknya fase pertama
+	*/
+	
+	/* FASE I */
+	if (word.word.match(/lah$/)){
+		word.word.replace(/lah$/, "");
+		word.removedSuffix = "lah";
+	}
+	else if (word.word.match(/kah$/)){
+		word.word.replace(/kah$/, "");
+		word.removedSuffix = "kah";
+	}
+	
+	/* FASE II */
+	if (word.word.match(/ku$/)){
+		word.word.replace(/ku$/, "");
+		word.removedSuffix = "ku";
+	}
+	else if (word.word.match(/mu$/)){
+		word.word.replace(/mu$/, "");
+		word.removedSuffix = "mu";
+	}
+	else if (word.word.match(/nya$/)){
+		word.word.replace(/nya$/, "");
+		word.removedSuffix = "nya";
+	}
 	
 	return word;
 }
