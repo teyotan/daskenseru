0ff559ace8936048ed1ca95a9ba530c47a519d82
DXGeneral <ofan.lubis@gmail.com>
2017-06-17 19:55:06 +0700

    Idham - suffixStemmer dan rootWordChecker selesai. Belum diuji

--- modules/preprocesser/stemmer/lib/suffixStemmer.js
+++ modules/preprocesser/stemmer/lib/suffixStemmer.js
@@ -5,32 +5,46 @@ var suffixStemmer = function(word){
 	Akan ada dua fase
 	Fase pertama adalah pengecekan dan penghapusan Inflection Suffix -lah dan -kah
 	Fase kedua adalah pengecekan dan penghapusan Inflectional Possessive Pronoun  (-ku, -mu, -nya)
+	Fase ketiga adalah pengecekan dan penghapusan Derivation Suffix (-i, -an, -kan)
+	
 	Fase kedua selalu dijalankan setelah fase pertama
 	Fase kedua dapat dijalankan tanpa perduli dijalankan atau tidaknya fase pertama
+	Fase ketiga selalu menyimpan suffix yang dihapus
 	*/
 	
 	/* FASE I */
 	if (word.word.match(/lah$/)){
 		word.word.replace(/lah$/, "");
-		word.removedSuffix = "lah" + word.removedSuffix;
 	}
 	else if (word.word.match(/kah$/)){
 		word.word.replace(/kah$/, "");
-		word.removedSuffix = "kah" + word.removedSuffix;
 	}
 	
 	/* FASE II */
 	if (word.word.match(/ku$/)){
 		word.word.replace(/ku$/, "");
-		word.removedSuffix = "ku" + word.removedSuffix;
 	}
 	else if (word.word.match(/mu$/)){
 		word.word.replace(/mu$/, "");
-		word.removedSuffix = "mu" + word.removedSuffix;
 	}
 	else if (word.word.match(/nya$/)){
 		word.word.replace(/nya$/, "");
-		word.removedSuffix = "nya" + word.removedSuffix;
+		// word.removedSuffix = "nya" + word.removedSuffix;
+	}
+	
+	/* FASE III */
+	if (word.word.match(/i$/)){
+		word.word.replace(/i$/, "");
+		word.removedSuffix = "i";
+	}
+	else if (word.word.match(/an$/)){
+		word.word.replace(/an$/, "");
+		word.removedSuffix = "an";
+	}
+	
+	else if (word.word.match(/kan$/)){
+		word.word.replace(/kan$/, "");
+		word.removedSuffix = "kan";
 	}
 	
 	return word;
