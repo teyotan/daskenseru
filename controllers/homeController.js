c9da573eab3723174e6c34001e64c99e9c90e02a
teyotan <theo.tanadi122@gmail.com>
2017-06-27 18:03:58 +0700

    performance fix, todo: stemmer fix for suffix -an -kan, template engine (kalo sempet)

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -7,16 +7,24 @@ const analyzer = require('../modules/analyzer').analyze
 const Layout = require("../views/components/layout.js")
 const Tokenized = require("../views/components/tokenized.js")
 
+const logger = require('winston').loggers.get('logger')
+
 const index = function(req, res, next) {
+	let startTime = new Date();
+
 	var arr = preprocesser(req.body.text);
+
+	let endTime = new Date();
 	
 	//var arr = analyzer(arr);
+	logger.info(req.body.text, (endTime - startTime)+'ms')
 
 	res.send(
 		ReactDOMServer.renderToString(
 			React.createElement(Layout, {tokenized: arr.tokenized, stemmed: arr.stemmed}, null)
 		)
 	)
+
 };
 
 exports.index = index;
